f480bf83ef5b37a245256bc6d86d7a5a7d89f1e0
Lonesum <kusharanjanbag@gmailz.com>
2026-01-04 17:55:03 +0530

    Linked MongoDB to Recommendation service

--- aiservice/aiservice/src/main/java/com/fitness/aiservice/model/Recommendation.java
+++ aiservice/aiservice/src/main/java/com/fitness/aiservice/model/Recommendation.java
@@ -1,7 +1,9 @@
 package com.fitness.aiservice.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -9,11 +11,12 @@ import org.springframework.data.mongodb.core.mapping.Document;
 import java.time.LocalDateTime;
 import java.util.List;
 
-@Document(collection = "recommendation")
+@Document(collection = "recommendations")
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Recommendation {
-
     @Id
     private String id;
     private String activityId;
@@ -26,6 +29,4 @@ public class Recommendation {
 
     @CreatedDate
     private LocalDateTime createdAt;
-
-
 }
